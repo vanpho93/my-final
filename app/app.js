d2d53f9dd43c9f641a65778517f445b6a2bdc8d2
Admin <vanpho02@gmail.com>
2017-01-20 18:06:46 +0700

    het video 3

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Router, Route, IndexRoute, hashHistory} = require('react-router');
+
 var HomePage = require('HomePage');
 var Nav = require('Nav');
 var Account = require('Account');
@@ -7,12 +9,12 @@ var Transaction = require('Transaction');
 var Main = require('Main');
 
 ReactDOM.render(
-  <div>
-    <Main/>
-    <Account/>
-    <Nav/>
-    <Transaction/>
-    <HomePage/>
-  </div>,
+  <Router history={hashHistory}>
+    <Router path="/" component={Main}>
+      <IndexRoute component={HomePage}/>
+      <Route path="account" component={Account}/>
+      <Route path="transaction" component={Transaction}/>
+    </Router>
+  </Router>,
   document.getElementById('root')
 );
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Nav from 'Nav';
 
 class Main extends React.Component{
   render(){
     return (
       <div>
         <h1>Main</h1>
+        <Nav/>
+        {this.props.children}
       </div>
     )
   }
