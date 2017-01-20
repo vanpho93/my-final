b59e7c8405c055ceb4af50d5127000b12ba5a895
Admin <vanpho02@gmail.com>
2017-01-20 18:03:11 +0700

    Het video 1

---
+++ app/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class List extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>This is Homepage</h1>
+      </div>
+    )
+  }
+}
+
+module.exports = List;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './app/app.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      HomePage: 'app/components/HomePage.js'
+    }
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
